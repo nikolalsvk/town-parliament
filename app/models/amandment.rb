b797a73a9dda91995d285d65b4aa84acfcb1ec6b
nikolalsvk <nikolaseap@gmail.com>
2016-06-14 17:09:05 +0200

    Change model

--- app/models/amandment.rb
+++ app/models/amandment.rb
@@ -1,4 +1,6 @@
 class Amandment < ActiveRecord::Base
+  TYPE = ["wait", "accepted", "declined"]
+
   belongs_to :owner, :polymorphic => true, :foreign_key => :owner_id
 
   belongs_to :user, -> { where(:name => "alderman") }
