39586d2655d61cb6f2592d3babe5d1cc6071199c
nikolalsvk <nikolaseap@gmail.com>
2016-06-16 23:06:03 +0200

    User can only access index page if he is not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_action :authenticate_user!
 
   def render404
     render :file => File.join(Rails.root, 'public', '404.html'), :status => 404, :layout => false
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,16 @@
 class PagesController < ApplicationController
 
+  before_filter :authenticate_user!, :except => [:index]
+
   def index
   end
+
   def meeting
     @meeting = Meeting.find(1)
-    @meeting.status = @meeting.status ? false : true
+    @meeting.status = params[:status] if params[:status]
     @meeting.save
     @acts = Act.all
     @amandments = Amandment.all
   end
+
 end
